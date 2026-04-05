2614cbde6948019a0d8e5109abba60ab18029365
dulanjananisal <dulanjananisal67@gmail.com>
2026-04-06 00:27:21 +0530

    create readme file for backend

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,19 +1,15 @@
 const Users = require('../models/userModels')
 const asyncErrorHaddler = require('../utils/asyncHaddler')
-const BadrequestErrorHaddler = require('../errors/BadrequestErrorHadder')
 
 //user register
 const userRegisterController = asyncErrorHaddler(async (req,res)=>{
     const registerUser = await Users.create(req.body)
-    if(!registerUser){
-         throw BadrequestErrorHaddler('Error')
-    }
     res.status(200).send(registerUser)
 })
 
 //login users
 const userLoginController = (req,res)=>{
-    res.status(200).send('Login users')
+    const userLogin = await 
 }
 
 module.exports = {
--- backend/middleware/errorHaddlerMiddlewre.js
+++ backend/middleware/errorHaddlerMiddlewre.js
@@ -2,10 +2,10 @@ const statusCodes = require('http-status-codes')
 
 const errorHaddlerMiddleware = (err,req,res,next)=>{
     let allErrors = {
-        messgae: err.message || 'Server error please try again later!',
+        message: err.message || 'Server error please try again later!',
         status: err.status || statusCodes.INTERNAL_SERVER_ERROR,
         code: err.code || 'INTERNAL_SERVER_ERROR'
     }
-    res.status(allErrors.status).json({success: false, message: allErrors.messgae, code: allErrors.code})
+    res.status(allErrors.status).json({success: false, message: allErrors.message, code: allErrors.code})
 }
 module.exports = errorHaddlerMiddleware;
