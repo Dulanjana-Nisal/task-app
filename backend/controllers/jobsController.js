33cbe1930bd35794a574a1ff751d4da31be6de1e
dulanjananisal <dulanjananisal67@gmail.com>
2026-04-06 14:42:14 +0530

    fix some bug in job controller

--- backend/controllers/jobsController.js
+++ backend/controllers/jobsController.js
@@ -2,27 +2,27 @@ const statusCodes = require('http-status-codes')
 
 //get all jobs
 const getAllJobs = (req,res)=>{
-    req.status(statusCodes.OK).send('Get all jobs')
+    res.status(statusCodes.OK).send('Get all jobs')
 }
 
 //create jobs
 const createJobs = (req,res)=>{
-    req.status(statusCodes.OK).send('Create jobs')
+    res.status(statusCodes.OK).send('Create jobs')
 }
 
 //get single jobs
 const getSingleJobs = (req,res)=>{
-    req.status(statusCodes.OK).send('Get single jobs')
+    res.status(statusCodes.OK).send('Get single jobs')
 }
 
 //update jobs
 const updateJobs = (req,res)=>{
-    req.status(statusCodes.OK).send('Update jobs')
+    res.status(statusCodes.OK).send('Update jobs')
 }
 
 //delete jobs
 const deleteJobs = (req,res)=>{
-    req.status(statusCodes.OK).send('Delete jobs')
+    res.status(statusCodes.OK).send('Delete jobs')
 }
 
 module.exports = {
