68cf54dcf3ccd08ed3b7c5363721105d825c0a2b
dulanjananisal <dulanjananisal67@gmail.com>
2026-04-07 21:16:09 +0530

    add all functionality in dashboard

--- frontend/task-app/src/pages/dashboard/dashboard.jsx
+++ frontend/task-app/src/pages/dashboard/dashboard.jsx
@@ -5,26 +5,93 @@ import editSvg from '../../assets/edit.png';
 import deleteSvg from '../../assets/delete.png';
 import api from '../../api/api';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
 
-    const [userData,setUserData] = useState([])
-    const [taskData,setTaskData] = useState([])
+    const [userData, setUserData] = useState([]);
+    const [taskData, setTaskData] = useState([]);
+    const [toggle, setToggle] = useState(false);
+    const [addTaskToggle, setAddTaskToggle] = useState(false);
+    const [updateTaskToggle, setUpdateTaskToggle] = useState(false);
+    const [addTaskName, setAddTaskName] = useState("");
+    const [addTaskDesc, setAddTaskDesc,] = useState("");
+    const [updateTaskId, setUpdateTaskId,] = useState("");
+    const [addTaskDone, setAddTaskDone,] = useState(false);
+    const [errMsg, setErrMsg] = useState(null)
+    const navigate = useNavigate();
 
-    useEffect( ()=>{
-        async function fetchTasks(){
-            try{
+    function profile() {
+        toggle ? setToggle(false) : setToggle(true);
+    }
+
+    function updateTask(e) {
+        setUpdateTaskId(e.target.id)
+        updateTaskToggle ? setUpdateTaskToggle(false) : setUpdateTaskToggle(true);
+    }
+
+    function addTask() {
+        addTaskToggle ? setAddTaskToggle(false) : setAddTaskToggle(true);
+    }
+
+    function userLogout() {
+        localStorage.removeItem('token');
+        navigate('/login')
+    }
+
+    //get all tasks
+    useEffect(() => {
+        async function fetchTasks() {
+            try {
                 const userTask = await api.get('/tasks')
                 setUserData(userTask.data.user)
                 setTaskData(userTask.data.data)
             }
-            catch(err){
+            catch (err) {
+                navigate('/login')
                 console.log(err)
             }
         }
         fetchTasks();
-    }, [])
-    console.log(taskData)
+    }, [addTaskToggle,updateTaskToggle])
+
+    //create task
+    const addTaskSubmit = async (e) => {
+        setAddTaskName("")
+        setAddTaskDesc("")
+        e.preventDefault();
+        try {
+            await api.post('/tasks', { "title": addTaskName, "description": addTaskDesc });
+            setAddTaskToggle(false)
+        }
+        catch (err) {
+            setErrMsg(err.response.data.message)
+        }
+    }
+
+    //update task
+    const updateTaskSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await api.patch(`/tasks/${updateTaskId}`, { "title": addTaskName, "description": addTaskDesc, "isDone": addTaskDone });
+            setUpdateTaskToggle(false)
+        }
+        catch (err) {
+            console.log(err.response)
+            setErrMsg(err.response.data.message)
+        }
+    }
+
+    //delete task
+    const deleteTask = async (id)=>{
+        try{
+            await api.delete(`/tasks/${id}`)
+            location.reload();
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
 
     return (
         <>
@@ -33,23 +100,26 @@ function Dashboard() {
                     <div class="header-left">
                         <h1>Dashboard</h1>
                     </div>
-                    <div class="header-right">
+                    <div class="header-right" onClick={profile}>
                         <img src={userSvg} alt="user-profile" />
                         <h1>{userData.name}</h1>
                     </div>
-                    <div class="profile">
-                        <p>{userData.email}</p>
-                        <button>Logout</button>
-                    </div>
+                    {
+                        toggle &&
+                        <div class="profile">
+                            <p>{userData.email}</p>
+                            <button onClick={userLogout}>Logout</button>
+                        </div>
+                    }
                 </div>
                 <div class="dashboard-body">
                     <div class="body-head">
-                        <button> <img src={plusSvg} alt="" />Create Task</button>
+                        <button onClick={addTask}> <img src={plusSvg} alt="" />Create Task</button>
                     </div>
                     <div class="body-bottom">
                         {
-                            taskData.map((tasks)=>{
-                                return(
+                            taskData.map((tasks) => {
+                                return (
                                     <div class="task-card" key={tasks._id}>
                                         <div class="card-head">
                                             <h1>{tasks.title}</h1>
@@ -58,8 +128,8 @@ function Dashboard() {
                                         </div>
                                         <div class="card-body">
                                             <div class="card-body-left">
-                                                <button class="edit-btn"><img src={editSvg} alt="" /></button>
-                                                <button class="delete-btn"><img src={deleteSvg} alt="" /></button>
+                                                <button class="edit-btn" onClick={updateTask} ><img src={editSvg} alt="" id={tasks._id} /></button>
+                                                <button class="delete-btn" onClick={() => {deleteTask(tasks._id)}}><img src={deleteSvg} alt="" /></button>
                                             </div>
                                             <div class="card-body-right">
                                                 <p>{tasks.updatedAt.slice(0, 10)}</p>
@@ -71,48 +141,60 @@ function Dashboard() {
                         }
                     </div>
                 </div>
-                <div class="add-task">
-                    <form action="#" method="post">
-                        <div class="name">
-                            <label for="name">Task Name</label>
-                            <input type="text" placeholder="Enter your Task Name" />
-                        </div>
-                        <div class="discription">
-                            <label for="name">Discription</label>
-                            <input type="text" placeholder="Enter your Discription" />
-                        </div>
-                        <div class="messages">
-                            <p>Some Error messages here</p>
-                        </div>
-                        <div class="btn">
-                            <input type="submit" value="Add" class="add-btn" />
-                            <button>cancel</button>
-                        </div>
-                    </form>
-                </div>
-                <div class="update-task">
-                    <form action="#" method="post">
-                        <div class="name">
-                            <label for="name">Task Name</label>
-                            <input type="text" placeholder="Enter your Task Name" />
-                        </div>
-                        <div class="discription">
-                            <label for="name">Discription</label>
-                            <input type="text" placeholder="Enter your Discription" />
-                        </div>
-                        <div class="isDone">
-                            <label for="name">Completed</label>
-                            <input type="checkbox" />
-                        </div>
-                        <div class="messages">
-                            <p>Some Error messages here</p>
-                        </div>
-                        <div class="btn">
-                            <input type="submit" value="Update" class="submit-btn" />
-                            <button>cancel</button>
-                        </div>
-                    </form>
-                </div>
+                {
+                    addTaskToggle &&
+                    <div class="add-task">
+                        <form onSubmit={addTaskSubmit}>
+                            <div class="name">
+                                <label for="name">Task Name</label>
+                                <input type="text" placeholder="Enter your Task Name" value={addTaskName} onChange={(e) => { setAddTaskName(e.target.value) }} />
+                            </div>
+                            <div class="discription">
+                                <label for="name">Discription</label>
+                                <input type="text" placeholder="Enter your Discription" value={addTaskDesc} onChange={(e) => { setAddTaskDesc(e.target.value) }} />
+                            </div>
+                            {
+                                errMsg &&
+                                <div class="messages">
+                                    <p>{errMsg}</p>
+                                </div>
+                            }
+                            <div class="btn">
+                                <input type="submit" value="Add" class="add-btn" />
+                                <button onClick={addTask}>cancel</button>
+                            </div>
+                        </form>
+                    </div>
+                }
+                {
+                    updateTaskToggle &&
+                    <div class="update-task">
+                        <form onSubmit={updateTaskSubmit}>
+                            <div class="name">
+                                <label for="name">Task Name</label>
+                                <input type="text" placeholder="Enter your Task Name" value={addTaskName} onChange={(e) => { setAddTaskName(e.target.value) }}/>
+                            </div>
+                            <div class="discription">
+                                <label for="name">Discription</label>
+                                <input type="text" placeholder="Enter your Discription" value={addTaskDesc} onChange={(e) => { setAddTaskDesc(e.target.value) }} />
+                            </div>
+                            <div class="isDone">
+                                <label for="name">Completed</label>
+                                <input type="checkbox" onChange={(e)=>{setAddTaskDone(e.target.checked)}}/>
+                            </div>
+                            {
+                                errMsg &&
+                                <div class="messages">
+                                    <p>{errMsg}</p>
+                                </div>
+                            }
+                            <div class="btn">
+                                <input type="submit" value="Update" class="submit-btn" />
+                                <button onClick={updateTask}>cancel</button>
+                            </div>
+                        </form>
+                    </div>
+                }
                 <div class="dashboard-footer"></div>
             </div>
         </>
